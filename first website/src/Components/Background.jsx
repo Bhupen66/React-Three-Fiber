f86644edd7deccef5954f27eecf937199ae4c4d2
Bhupen66 <chauhanbhupen66@gmail.com>
2024-12-22 05:32:44 +0530

    refactor Background component to use lamina for gradient and update sphere properties

--- first website/src/Components/Background.jsx
+++ first website/src/Components/Background.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import {Environment, Sphere} from '@react-three/drei'
 import * as THREE from 'three'
-import {LayerMaterial, Gradient} from '@react-three/drei'
+import { LayerMaterial, Gradient } from "lamina"
 
 function Background() {
   return (
     <>
-        <Environment />
-        <Sphere args={[100, 100, 100]} rotation-y={Math.PI / 2}>
+        <Environment preset="sunset" />
+        <Sphere scale={[100, 100, 100]} rotation-y={Math.PI / 2}>
             <LayerMaterial 
             lighting="physical"
             tranmission={1}
             side={THREE.BackSide}
             >
                 <Gradient 
-                colorA={"#61b4e8"} 
-                coloeB={"#61b4e8"}   
-                axes={"y"}
+                colorA={"blue"}
+                colorB={"red"}
+                axes={"x"}
                 start={0}
-                end={0.5}
+                end={-0.5}
                 />
             </LayerMaterial>
         </Sphere>
