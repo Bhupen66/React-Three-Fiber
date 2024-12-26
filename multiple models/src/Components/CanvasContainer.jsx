7859f3b01e9bbdcb3ea3a5a91a3fd24aebe888a8
Bhupen66 <chauhanbhupen66@gmail.com>
2024-12-26 21:13:06 +0530

    feat: enhance CanvasContainer to render a 3D box using @react-three/fiber

--- multiple models/src/Components/CanvasContainer.jsx
+++ multiple models/src/Components/CanvasContainer.jsx
@@ -1,9 +1,18 @@
+import { Canvas } from '@react-three/fiber';
 import React from 'react'
 
 function CanvasContainer() {
-  return (
-    <div>CanvasContainer</div>
-  )
+    return (
+        <div className='canvas-container w-full h-screen'> 
+
+            <Canvas>
+                <mesh>
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial color='hotpink' />
+                </mesh>
+            </Canvas>
+        </div>
+    )
 }
 
-export default CanvasContainer
+export default CanvasContainer;
